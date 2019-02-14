4b5a21fe46019cffb9e7f556d99bd619a75bde03
John W. Ellis <jellis@yieldbot.com>
2019-02-14 07:36:28 -0500

    language server and stuff

--- personal/dot.emacs.el
+++ personal/dot.emacs.el
@@ -55,3 +55,42 @@
 ;; Nvm
 (require 'nvm)
 (add-to-list 'auto-mode-alist '("\\.js?\\'" . nvm))
+
+;; For Java Language Server Protocol (LSP) mode
+;; https://github.com/emacs-lsp/lsp-java
+(require 'cc-mode)
+
+(condition-case nil
+    (require 'use-package)
+  (file-error
+   (require 'package)
+   (add-to-list 'package-archives '("melpa" . "http://melpa.org/packages/"))
+   (package-initialize)
+   (package-refresh-contents)
+   (package-install 'use-package)
+   (require 'use-package)))
+
+(use-package projectile :ensure t)
+(use-package treemacs :ensure t)
+(use-package yasnippet :ensure t)
+(use-package lsp-mode :ensure t)
+(use-package hydra :ensure t)
+(use-package company-lsp :ensure t)
+(use-package lsp-ui :ensure t)
+(use-package lsp-java :ensure t :after lsp
+             :config (add-hook 'java-mode-hook 'lsp))
+
+(use-package dap-mode
+             :ensure t :after lsp-mode
+             :config
+             (dap-mode t)
+             (dap-ui-mode t))
+
+(use-package dap-java :after (lsp-java))
+(use-package lsp-java-treemacs :after (treemacs))
+
+(require 'lsp-java)
+(add-hook 'java-mode-hook #'lsp)
+
+(setq lsp-java-workspace-dir "/home/elljoh/dev/projects/jdt-lsp-workspace/")
+(setq lsp-java-workspace-cache-dir "/home/elljoh/dev/projects/jdt-lsp-workspace/")
